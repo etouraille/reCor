b036317da828ce2f2e1d2792f6a28c1b37bb60ec
Édouard Touraile <edouard.touraille@gmail.com>
2015-09-01 17:39:01 +0200

    CO

--- www/js/config/settings.js
+++ www/js/config/settings.js
@@ -1,10 +1,10 @@
-app.constant('settings', {
+pp.constant('settings', {
     encryption : {
         cycleNumber : 2
     },
     tag : 'freePress',
-    //endpoint : 'http://resource/app_dev.php/',
-    endpoint : 'http://resource.objetspartages.org/app_dev.php/',
+    endpoint : 'http://resource/app_dev.php/',
+    //endpoint : 'http://resource.objetspartages.org/app_dev.php/',
     cdn : 'http://fanny.objetspartages.org/',
     androidProjectNumber : '672537723017', 
     messageType : 'pneumatique',
